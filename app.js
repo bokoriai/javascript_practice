8825e0d058159b0ec56f6da69f90862d25f448f6
bokoriai <139733346+bokoriai@users.noreply.github.com>
2023-12-13 06:06:17 +0000

    ２章確認問題

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-let max = 100;
-let num = 1;
-let count = 0;
+let num1 = 5;
+let num2 = 5;
 
-while(num < max){
-    num = num * 2;
-    count = count + 1;
-}
-
-alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+alert(num1 + num2);
+alert(num1 - num2);
+alert(num1 * num2);
+alert(num1 / num2);

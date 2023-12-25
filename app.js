bf316d9bf3e10daae1019fad2911740421ea771d
bokoriai <139733346+bokoriai@users.noreply.github.com>
2023-12-25 12:49:35 +0000

    java3章　作成した関数を呼び出す

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-let num1 = 5;
-let num2 = 5;
+//メイン部分
+let alertString;
 
-alert(num1 + num2);
-alert(num1 - num2);
-alert(num1 * num2);
-alert(num1 / num2);
+//作成した関数を呼び出し、変数へ格納
+alertString = addString("WebCamp");
+
+//変数の中身をアラートで表示する
+alert(alertString);
+
+//作成した関数
+function addString(strA){
+    let addStr = "Hello" + strA;
+    return addStr;
+}
